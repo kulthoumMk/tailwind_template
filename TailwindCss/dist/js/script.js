294e426cf3f4146cc38922f1c0ac97764db94194
mustafa_kulthoum <mustafa.2420282@std.stardomuniversity.edu.eu>
2025-05-21 22:11:31 +0200

    no message

---
+++ TailwindCss/dist/js/script.js
@@ -0,0 +1,6 @@
+const mobilBtn=document.querySelector("#mobile_btn")
+const mobilMenu=document.querySelector("#mobile_menu")
+
+mobilBtn.addEventListener('click',()=>{
+mobilMenu.classList.toggle('hidden');
+});
